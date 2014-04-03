933fea689605421ba5a561e4e930452ef31366e3
Abhijeet K <user@email.com>
2014-04-03 18:19:41 +0530

    Finish static pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+  
+  def decideTiele(title)
+    baseName = 'RoR Sample App'
+    if title.empty?
+      baseName
+    else
+      "#{baseName} | #{title}"
+    end
+  end
+  
 end
